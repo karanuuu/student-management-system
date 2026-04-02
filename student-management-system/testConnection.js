f9b8325c233d7bbd1fde56bc501ab2478ddda948
S-t-a-n-n <stanleymoga005@gmail.com>
2026-04-02 22:17:22 +0300

    fix: remove other members files from my branch

--- student-management-system/testConnection.js
+++
@@ -1,7 +0,0 @@
-const connection = require('./config/db'); // uses your db.js setup
-
-connection.query('SELECT 1', (err, results) => {
-  if (err) throw err;
-  console.log('Database connection works:', results);
-  connection.end();
-});
