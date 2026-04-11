73a44418d9cd431d7d38f298d5cdf410b88b01ee
Stanley Moga <stanleymoga005@gmail.com>
2026-04-11 17:24:55 +0300

    Implement CORS and enhance base route message
    Added CORS middleware and updated base route response.

--- server.js
+++ server.js
@@ -1,23 +1,24 @@
 const express = require("express");
+const cors = require('cors');
 const dotenv = require("dotenv");
-
-// Load environment variables
-dotenv.config();
 
 const app = express();
 
 // Middleware to parse JSON
+app.use(cors());
 app.use(express.json());
-const authRoutes = require("./routes/authRoutes");
-app.use("/api/auth", authRoutes);
 
-// Simple test route
-app.get("/", (req, res) => {
-  res.send("Server is running...");
+app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/tasks', require('./routes/taskRoutes'));
+
+
+// Base route
+app.get('/', (req, res) => {
+  res.json({ message: 'Student Assignment Manager API is running!' });
 });
 
 // Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
